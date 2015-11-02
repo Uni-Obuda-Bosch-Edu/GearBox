b504601cde38a9d8aa48d3c175d3abc92160a981
Szloboda Dániel <sz.danix6@gmail.com>
2015-11-02 17:03:26 +0100

    Gearbox finding fix

--- src/busInterface/Gearbox_Out.java
+++
@@ -1,7 +0,0 @@
-package busInterface;
-
-public interface Gearbox_Out {
-	public void setGearTorque(double gearTorque);
-	public void setGearRevolution(int gearRevolution);
-	public void setGearMode(int gearMode);
-}
